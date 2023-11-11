087db944f0cc463fdd6ad6b03bad32b6c3d19ce3
V1s1t@nt <victorsoslatin@gmail.com>
2023-11-11 11:42:58 -0600

    First

---
+++ test.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:V1s1t@nt@localhost/DB_Bank'
+db = SQLAlchemy(app)
+
+print("Flask y SQLAlchemy corren como el viento.")
