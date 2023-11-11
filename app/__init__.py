1e48466c57cb3c1e0b713c683cdfa64094028da1
V1s1t@nt <victorsoslatin@gmail.com>
2023-11-11 14:53:10 -0600

    update de directorios, miss implementacion de modelos pra las rutas

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:V1s1t@nt@localhost/DB_Bank'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+
 db = SQLAlchemy(app)
 
-import DB_PRACTICE.models 
+
+from app.modelos import Agencia, Agente, Cliente, Prestamo, Pago
+
+
+from app import rut
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:V1s1t@nt@localhost/DB_Bank'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
