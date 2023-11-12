d6d4117151be2290a95422ed83cd7ce9482236ca
V1s1t@nt <victorsoslatin@gmail.com>
2023-11-11 18:43:31 -0600

    update de rutas con modelos implemenados

--- app/routes.py
+++ app/routes.py
@@ -91,3 +91,129 @@ def delete_agente(id):
     db.session.delete(agente)
     db.session.commit()
     return jsonify({'mensaje': 'Agente eliminado'})
+
+
+
+# Obtener todos los clientes
+@app.route('/clientes', methods=['GET'])
+def get_clientes():
+    clientes = Cliente.query.all()
+    return jsonify([cliente.to_dict() for cliente in clientes])
+
+# Obtener un cliente por su ID
+@app.route('/clientes/<int:id>', methods=['GET'])
+def get_cliente(id):
+    cliente = Cliente.query.get_or_404(id)
+    return jsonify(cliente.to_dict())
+
+# Crear un nuevo cliente
+@app.route('/clientes', methods=['POST'])
+def create_cliente():
+    data = request.json
+    nuevo_cliente = Cliente(nombre=data['nombre'], informacion_contacto=data.get('informacion_contacto'))
+    db.session.add(nuevo_cliente)
+    db.session.commit()
+    return jsonify(nuevo_cliente.to_dict()), 201
+
+# Actualizar un cliente existente
+@app.route('/clientes/<int:id>', methods=['PUT'])
+def update_cliente(id):
+    cliente = Cliente.query.get_or_404(id)
+    data = request.json
+    cliente.nombre = data.get('nombre', cliente.nombre)
+    cliente.informacion_contacto = data.get('informacion_contacto', cliente.informacion_contacto)
+    db.session.commit()
+    return jsonify(cliente.to_dict())
+
+# Eliminar un cliente
+@app.route('/clientes/<int:id>', methods=['DELETE'])
+def delete_cliente(id):
+    cliente = Cliente.query.get_or_404(id)
+    db.session.delete(cliente)
+    db.session.commit()
+    return jsonify({'mensaje': 'Cliente eliminado'})
+
+
+# Obtener todos los préstamos
+@app.route('/prestamos', methods=['GET'])
+def get_prestamos():
+    prestamos = Prestamo.query.all()
+    return jsonify([prestamo.to_dict() for prestamo in prestamos])
+
+# Obtener un préstamo por su ID
+@app.route('/prestamos/<int:id>', methods=['GET'])
+def get_prestamo(id):
+    prestamo = Prestamo.query.get_or_404(id)
+    return jsonify(prestamo.to_dict())
+
+# Crear un nuevo préstamo
+@app.route('/prestamos', methods=['POST'])
+def create_prestamo():
+    data = request.json
+    cliente = Cliente.query.get_or_404(data['cliente_id'])
+    nuevo_prestamo = Prestamo(monto=data['monto'], fecha_inicio=data['fecha_inicio'], estado=data.get('estado'), cliente_id=cliente.id)
+    db.session.add(nuevo_prestamo)
+    db.session.commit()
+    return jsonify(nuevo_prestamo.to_dict()), 201
+
+# Actualizar un préstamo existente
+@app.route('/prestamos/<int:id>', methods=['PUT'])
+def update_prestamo(id):
+    prestamo = Prestamo.query.get_or_404(id)
+    data = request.json
+    prestamo.monto = data.get('monto', prestamo.monto)
+    prestamo.fecha_inicio = data.get('fecha_inicio', prestamo.fecha_inicio)
+    prestamo.estado = data.get('estado', prestamo.estado)
+    db.session.commit()
+    return jsonify(prestamo.to_dict())
+
+# Eliminar un préstamo
+@app.route('/prestamos/<int:id>', methods=['DELETE'])
+def delete_prestamo(id):
+    prestamo = Prestamo.query.get_or_404(id)
+    db.session.delete(prestamo)
+    db.session.commit()
+    return jsonify({'mensaje': 'Préstamo eliminado'})
+
+
+# Obtener todos los pagos
+@app.route('/pagos', methods=['GET'])
+def get_pagos():
+    pagos = Pago.query.all()
+    return jsonify([pago.to_dict() for pago in pagos])
+
+# Obtener un pago por su ID
+@app.route('/pagos/<int:id>', methods=['GET'])
+def get_pago(id):
+    pago = Pago.query.get_or_404(id)
+    return jsonify(pago.to_dict())
+
+# Crear un nuevo pago
+@app.route('/pagos', methods=['POST'])
+def create_pago():
+    data = request.json
+    prestamo = Prestamo.query.get_or_404(data['prestamo_id'])
+    agente = Agente.query.get_or_404(data['agente_id'])
+    nuevo_pago = Pago(monto=data['monto'], fecha_pago=data['fecha_pago'], prestamo_id=prestamo.id, agente_id=agente.id, tarifa=data.get('tarifa'))
+    db.session.add(nuevo_pago)
+    db.session.commit()
+    return jsonify(nuevo_pago.to_dict()), 201
+
+# Actualizar un pago existente
+@app.route('/pagos/<int:id>', methods=['PUT'])
+def update_pago(id):
+    pago = Pago.query.get_or_404(id)
+    data = request.json
+    pago.monto = data.get('monto', pago.monto)
+    pago.fecha_pago = data.get('fecha_pago', pago.fecha_pago)
+    pago.tarifa = data.get('tarifa', pago.tarifa)
+    db.session.commit()
+    return jsonify(pago.to_dict())
+
+# Eliminar un pago
+@app.route('/pagos/<int:id>', methods=['DELETE'])
+def delete_pago(id):
+    pago = Pago.query.get_or_404(id)
+    db.session.delete(pago)
+    db.session.commit()
+    return jsonify({'mensaje': 'Pago eliminado'})
